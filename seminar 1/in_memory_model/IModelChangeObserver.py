1042823629756fcffd3e45ead38da0d1c603c778
Realmarvin17 <zverevdm@mail.ru>
2023-08-13 11:52:44 +0300

    add homework seminar 1

---
+++ seminar 1/in_memory_model/IModelChangeObserver.py
@@ -0,0 +1,8 @@
+from abc import ABC, abstractmethod
+
+
+class IModelChangeObserver(ABC):
+    @abstractmethod
+    def aplly_update_model() -> None:
+        pass
+    
---
+++ seminar 1/model_elements/Poligon.py
@@ -0,0 +1,5 @@
+class Poligon:
+    def __init__(self, points: Point3D) -> None:
+        self.points = points
+
+        
