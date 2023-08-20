a1b95f70b69afe9652e4a5cd61b97888c2b3f643
Realmarvin17 <zverevdm@mail.ru>
2023-08-20 12:02:00 +0300

    add SOLID_Design_Principles homework seminar 3

---
+++ seminar 3/Interface_Segregation_Principle/Interface_Segregation_Principle_Wrong_Implementation.py
@@ -0,0 +1,48 @@
+from abc import ABCMeta, abstractmethod
+
+# Interface Substitution Principle
+# No client should be forced to depend on methods it does not use
+'''
+A larger interface bundled with all methods makes it mandatory for all classes to provide implementation of all the methods even if 
+some methods are not relevant to them
+'''
+
+class CommunicationDevice():
+  @abstractmethod
+  def make_calls():
+    pass
+
+  @abstractmethod
+  def send_sms():
+    pass
+
+  @abstractmethod
+  def browse_internet():
+    pass
+
+class SmartPhone(CommunicationDevice):
+  def make_calls():
+    #implementation
+    pass
+
+  def send_sms():
+    #implementation
+    pass
+
+  def browse_internet():
+    #implementation
+    pass
+
+  class LandlinePhone(CommunicationDevice):
+    def make_calls():
+      #implementation
+      pass
+
+    def send_sms():
+      #just pass or raise exception as this feature is not supported
+      pass
+
+    def browse_internet():
+      #just pass or raise exception as this feature is not supported
+      pass
+
---
+++ seminar 3/Open_Closed_Principle/Open_Close_Principle_Wrong_Implementation.py
@@ -0,0 +1,35 @@
+# Open - Close Principle
+# Software entities (classes, modules, functions, etc.) should be open for extension, but closed for modification
+'''
+This design breaches the Open Closed principle because this class will need modification if
+a). A new apparel type is to be included and
+b). If the discount amount for any apparel changes
+'''
+from enum import Enum
+class Products(Enum):
+  SHIRT = 1
+  TSHIRT = 2
+  PANT = 3
+
+class DiscountCalculator():
+  def __init__(self, product_type, cost):
+    self.product_type = product_type
+    self.cost = cost
+
+  def get_discounted_price(self):
+    if self.product_type == Products.SHIRT:
+      return self.cost - (self.cost * 0.10)
+    elif self.product_type == Products.TSHIRT:
+      return self.cost - (self.cost * 0.15)
+    elif self.product_type == Products.PANT:
+      return self.cost - (self.cost * 0.25)
+
+dc_Shirt = DiscountCalculator(Products.SHIRT, 100)
+print(dc_Shirt.get_discounted_price())
+
+dc_TShirt = DiscountCalculator(Products.TSHIRT, 100)
+print(dc_TShirt.get_discounted_price())
+
+dc_Pant = DiscountCalculator(Products.PANT, 100)
+print(dc_Pant.get_discounted_price())
+
